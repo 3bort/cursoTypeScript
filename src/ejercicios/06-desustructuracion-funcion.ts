b8fdb84f49dce42ab1fff4eefc4bea9bc6caaeef
3bort <e.bortm@gmail.com>
2021-01-27 00:42:06 +0100

    Desestructuración Funciones

---
+++ src/ejercicios/06-desustructuracion-funcion.ts
@@ -0,0 +1,54 @@
+//Desestructuración funciones
+
+interface Producto {
+    desc: string,
+    precio: number,
+}
+
+const telefono: Producto = {
+    desc: 'Nokia A1',
+    precio: 150
+}
+
+const tablet: Producto = {
+    desc: 'iPad Air',
+    precio: 350
+}
+
+function calculaISV( productos: Producto[]) :number {
+    let total = 0;
+    productos.forEach((producto) =>{
+        total += producto.precio;
+    })
+
+    return total * 0.15;
+}
+
+const articulos = [ telefono, tablet];
+
+const isv = calculaISV(articulos);
+
+console.log('ISV:', isv)
+
+//Desustructuración
+
+
+function calculaISVD( productos: Producto[]) :[number, number] {
+    let totald = 0;
+    productos.forEach(({precio}) =>{
+        totald += precio;
+    })
+
+    return [totald, totald * 0.15];
+}
+
+const articulosd = [ telefono, tablet];
+
+const [totald, isvd] = calculaISVD(articulosd);
+
+console.log('ISVD:', isvd)
+console.log('TotalD:', totald)
+
+
+
+
