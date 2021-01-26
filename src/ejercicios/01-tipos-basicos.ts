d05ec0c85db6f7c13d3db95a23a6c78d9ed571f7
3bort <e.bortm@gmail.com>
2021-01-26 15:21:18 +0100

    Introducción

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+const nick = "3Bort";
+let nombre: string = "3Bort";
+let hp: number | string = 95; // Con esto podriamos tener dos tipos de datos
+let estaVivo: boolean = true;
+
+nombre = "Emilio"; // No podemos meter números por lo asignado
+hp = "FULL";
+
+console.log(nombre, hp, estaVivo);
+
